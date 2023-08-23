5c899ebf5f7f81e364c7c9e19579517c3c70bc36
klvgk <klv.gonzaga@gmail.com>
2023-08-23 18:24:21 -0300

    Projeto para tocar o instrumento Bateria através do Teclado.

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+//Pega o evento do teclado;
+document.body.addEventListener('keyup', (event) => {
+    playSound(event.code.toLowerCase());
+
+});
+
+//Audita o botão para quando for clicado para tocar uma composição;
+document.querySelector('.composer button').addEventListener('click', () => {
+
+    let song = document.querySelector('#input').value;
+
+    if (song !== '') {
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+});
+
+//Toca o som;
+function playSound(sound) {
+
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+    if (audioElement) {
+        audioElement.currentTime = 0;
+        audioElement.play();
+    }
+
+    if (keyElement) {
+        keyElement.classList.add('active');
+
+        setTimeout(() => {
+            keyElement.classList.remove('active');
+        }, 300);
+    }
+
+};
+
+//Toca a composição;
+function playComposition(songArray) {
+
+    let wait = 0;
+
+    for (let songItem of songArray) {
+
+        setTimeout(() => {
+            playSound(`key${songItem}`);
+        }, wait);
+
+
+        wait += 250;
+        console.log(wait);
+    }
+}
